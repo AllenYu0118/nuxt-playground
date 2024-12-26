4bfbfbae471938cdf03c1c549f663c70b7dcbbd3
allenyu <yulei0118@gmail.com>
2024-12-26 09:41:11 +0800

    feat: player upgrade

--- components/Player.client.vue
+++ components/Player.client.vue
@@ -1,18 +1,19 @@
 <template>
-  <T5Player :id pattern="component" src="https://video.591.com.tw/debug/target/house/hls/2023-04-12/6800/master.m3u8"
-    poster="https://video.591.com.tw/debug/target/house/cover/2023-04-12/6800.png" />
+  <div>
+    <T5Player :id pattern="component" src="https://video.591.com.tw/debug/target/house/hls/2023-04-12/6800/master.m3u8"
+      poster="https://video.591.com.tw/debug/target/house/cover/2023-04-12/6800.png" />
+
+    <div @click="togglePlayingStatus()">
+      播放
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { T5Player, useVideoControls } from '@t5/player'
-import '@t5/player/style'
 
 const props = defineProps<{
   id: string
 }>()
-
-console.log('props.id: ', props.id);
-
-
 useVideoControls(props.id)
 </script>
